285fbe3ecad47899dab52e2441821b81e1648ae3
Rishi <rishi01prince@gmail.com>
2025-05-02 23:55:48 +0530

    add all files in the codebase

---
+++ js/data.js
@@ -0,0 +1,35 @@
+// content.js
+export const heroContent = {
+    title: "Education and Online Course Site Template",
+    description: "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam diam voluptua.",
+    buttonText: "Courses"
+};
+
+export const skillsContent = {
+    title: "Learn New Skills",
+    description: "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt utlabo",
+    skills: [{
+            title: "Learn Anything",
+            icon: "lni-pencil-alt",
+            description: "Lorem ipsum dolor sit amet, consetetu sadi ps cinelitr, sed diam nonumy eirtem Lorem ipsum dolor sit amet, consetetur."
+        },
+        // Add more skills as needed
+    ]
+};
+
+export const coursesContent = {
+    title: "Popular Courses",
+    description: "eStore UI Kit comes with all essential features, sections and elements you need to design and develop web UI efficiently.",
+    courses: [{
+            title: "Full-stack Web Development",
+            image: "../assets/images/course/full-stack.jpg",
+            price: "$99.00",
+            stats: {
+                users: "3.5k",
+                views: "12k",
+                rating: "5.0"
+            }
+        },
+        // Add more courses as needed
+    ]
+};
